e78b7d9bcae3cc78aeb6827503ac869105a82859
Norbert Forgacs <norbert.forgacs@amdaris.com>
2024-06-13 15:49:18 +0300

    terraform

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # Use the latest stable version
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+  name     = "tfmainrg"
+  location = "Germany West Central"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                = "weatherapi"
+  location            = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+
+  ip_address_type = "Public"
+  dns_name_label  = "forgacsbertwa"
+  os_type         = "Linux"
+
+  container {
+    name   = "weatherapi"
+    image  = "forgacsbert/weatherapi"
+    cpu    = "1"
+    memory = "1"
+
+    ports {
+      port     = 8080
+      protocol = "TCP"
+    }
+  }
+}
